0f29150b103e98de292851a4ee08e7cc09811eda
harshavardhan-macha <harshavardhanmacha19@gmail.com>
2025-09-14 16:24:11 +0530

    added the app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Dashboard from "./Dashboard";
+import './App.css'
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [selectedPage, setSelectedPage] = useState('Dashboard');
